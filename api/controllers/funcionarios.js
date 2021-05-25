9a7ee127b30ac328c30bc47a67ee988134f3ad28
Bruno Silva <brs.rsilva@gmail.com>
2021-05-25 13:35:20 -0300

    config api, routes and database(JSON)

---
+++ api/controllers/funcionarios.js
@@ -0,0 +1,35 @@
+module.exports = () => {
+    const funcionarios = require('../data/funcionarios.json');
+    const controller = {};
+  
+    controller.listFuncionarios = (req, res) => res.status(200).json(funcionarios);
+
+
+    controller.funcionarioId = (req, res) => {
+        let func = funcionarios.data.find(e => e.id === req.params.id)
+        res.status(200).json(func)
+    }
+
+    controller.funcionarioName = (req, res) => {
+        let func = funcionarios.data.find(e => e.name === req.params.name)
+        res.status(200).json(func)
+    }
+
+    controller.funcionarioCpf = (req, res) => {
+        let func = funcionarios.data.find(e => e.cpf === req.params.cpf)
+        res.status(200).json(func)
+    }
+
+    controller.funcionarioCargo = (req, res) => {
+        let func = funcionarios.data.find(e => e.cargo === req.params.cargo)
+        res.status(200).json(func)
+    }
+
+    controller.funcionarioAtivos = (req, res) => {
+        let func = funcionarios.data.filter(e => e.status === req.params.status.toUpperCase())
+        res.status(200).json(func)
+    }
+
+
+    return controller;
+  }
---
+++ api/routes/funcionarios.js
@@ -0,0 +1,23 @@
+module.exports = app => {
+    const controller = require('../controllers/funcionarios')();
+  
+    const defaultRoute = '/api/v1/funcionarios';
+
+    app.route(defaultRoute)
+        .get(controller.listFuncionarios);
+
+    app.route(`${defaultRoute}/:id`)
+        .get(controller.funcionarioId);
+
+    app.route(`${defaultRoute}/name/:name`)
+        .get(controller.funcionarioName);
+
+    app.route(`${defaultRoute}/cpf/:cpf`)
+        .get(controller.funcionarioCpf);
+
+    app.route(`${defaultRoute}/cargo/:cargo`)
+        .get(controller.funcionarioCargo);
+
+    app.route(`${defaultRoute}/status/:status`)
+        .get(controller.funcionarioAtivos);
+  }
